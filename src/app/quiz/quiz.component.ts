0b7d294238c4660d3f13e776e0180becf914ab9f
ioanh <ioan.xristov@abv.bg>
2021-09-01 22:11:04 +0300

    displaying the question

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quizAPI.service';
+import { Quiz } from './quiz.model';
 
 @Component({
   selector: 'app-quiz',
@@ -10,10 +11,16 @@ export class QuizComponent implements OnInit {
 
   constructor(private quizService: QuizService) { }
 
+  questions: Array<Quiz>;
+  question: Quiz;
+
   ngOnInit(): void {
     this.quizService.getQuestions().subscribe(
       (data) => {
         console.log(data)
+        this.questions = data.results
+        console.log(this.questions)
+        this.question = this.questions[0]
       }
     )
   }
---
+++ src/app/quiz/quiz.model.ts
@@ -0,0 +1,8 @@
+export class Quiz {
+    category: string;
+    type: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: Array<string>;
+}
