d148ba281b83fb122d304b137c11107938970620
ioanh <ioan.xristov@abv.bg>
2021-09-02 15:58:58 +0300

    removing not needed code

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -21,31 +21,19 @@ export class QuizComponent implements OnInit {
   ngOnInit(): void {
     this.quizService.getQuestions().subscribe(
       (data) => {
-        console.log(data)
         this.questions = data.results
-        console.log(this.questions)
         this.question = this.questions[0]
         this.progress = 100;
+
         this.source.subscribe((val) => {
           this.question = this.questions[val + 1]
           this.progress = 110;
         })
+
         this.progressTimer.subscribe(val => this.progress -= 10)
       }
     )
 
-  }
-
-  
-  previousQ(){
-    if(this.questions.indexOf(this.question) !== 0){
-      const pQ = this.questions.indexOf(this.question) - 1
-      this.question = this.questions[pQ]
-      this.progress -= 10;
-    }else {
-      this.question = this.question
-      this.progress = this.progress
-    }
   }
 
   nextQ(){
